26a968147f64002367f721005d7583a857c1cb3f
novo <weixiong_zhu@163.com>
2023-01-30 16:58:57 +0800

    feat: 前端搭建

---
+++ wobbs-frontend/src/service/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "/api/";
+axios.interceptors.request.use((config) => {
+    let loginResult = JSON.parse(localStorage.getItem("loginResult"));
+    if (loginResult) {
+        const token = loginResult.access_token;
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(
+    response => {
+        if (response.status === 200) {
+            return Promise.resolve(response.data);
+        } else {
+            return Promise.reject(response.data);
+        }
+    },
+    (error) => {
+        console.log('error', error);
+    }
+);
+
+export default axios;
