b9a13c0a51eb3ae98279622a5192235dd15488bf
novo <weixiong_zhu@163.com>
2023-02-03 22:32:49 +0800

    test: 添加测试

--- wobbs-server/test/limit_test.go
+++ wobbs-server/test/limit_test.go
@@ -1,1 +1,23 @@
 package test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"go.uber.org/ratelimit"
+)
+
+func TestLimit(t *testing.T) {
+	// 一秒多少滴水
+	limiter := ratelimit.New(1)
+	prev := time.Now()
+	for i := 0; i < 10; i++ {
+		now := limiter.Take()
+		if now.Sub(time.Now()) > 0 {
+			fmt.Println("ttt")
+		}
+		fmt.Println(i, now.Sub(prev))
+		prev = now
+	}
+}
